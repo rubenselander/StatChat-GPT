a040c934fb1a718e074541852b09aa82daef22c9
Ruben Selander <77073569+Reversehobo@users.noreply.github.com>
2024-01-31 23:49:33 +0100

    Update Article.md and ToDo.md, Fix encoding issue in json data, and modify api_search.py

--- ToDo.md
+++ ToDo.md
@@ -26,6 +26,8 @@
 - Write the instructions for our GPT to use the local api to find and retrieve data from Eurostat API (very simple just need to get the other parts done first)
 - DEMO IS DONE AND SHOULD NOW BE READY TO GO!
 
+# SMALL FIXES
+- Change all loading and dumping of json and jsonl data to use encoding="utf-8" and ensure_ascii=False. This to avoid issues with special characters such as in the country name "TR": "T�rkiye"
 
 # Further work
 - Write deep memory training data. We need X number of question and dataset title pairs. The questions should be something like "How has the average life expectancy in Finland changed for men and woman since 2000?" title should be something like "Life expectancy by country, gender and year". Note: The titles MUST be real titles from our collection.
--- scripts/api_search.py
+++ scripts/api_search.py
@@ -9,7 +9,7 @@ import os
 co = cohere.Client(os.environ["COHERE_API_KEY"])
 ORG_NAME = "rubenselander"  # Organization name on activeloop hub
 VECTOR_STORE_NAME = "eurostat"  # Name of vector store on activeloop hub
-TOKEN = os.environ["ACTIVATELOOP_TOKEN"]
+# TOKEN = os.environ["ACTIVATELOOP_TOKEN"]
 
 
 vector_store = VectorStore(
@@ -92,11 +92,32 @@ def process_search_results(search_string, results: dict, k: int):
     return new_results
 
 
-def search_eurostat(search_string: str, year: int = None) -> list[dict]:
+def search_eurostat(search_string: str, year: int = None, k=10) -> dict:
     """Performs a search in Eurostat based on the given search string."""
-    search_results = search_tables(search_string)
+    search_results = search_tables(search_string, k=k)
+    return search_results
 
 
+def test():
+    search_string = "Does life expectancy in the EU correlate with GDP per capita?"
+    search_results = search_eurostat(search_string, k=2)
+    print(f"type: {type(search_results)}")
+    # print(f"search_results: \n{search_results}")
+    # save to search_test.json
+    if isinstance(search_results, str):
+        search_results = json.loads(search_results)
+
+    # pop the embedding, id and source fields, the search results obj is a dict
+    search_results.pop("embedding")
+    search_results.pop("id")
+    search_results.pop("source")
+
+    with open("search_test.json", "w") as f:
+        json.dump(search_results, f, indent=4, ensure_ascii=False)
+
+
+if __name__ == "__main__":
+    test()
 # def __main__():
 #     search_string = "Har det blivit vanligare med tidiga skilsmässor?"
 #     search_string = "Vad tjänar en polis i snitt per månad?"
